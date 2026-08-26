fd2306771f5b4b5608a0b4913be34c5d2f4ed81f
Parity <trushildhokiya@gmail.com>
2026-08-26 07:31:15 +0530

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,6 +25,8 @@ return {
     -- The two essential shortcuts
     vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope open buffers' })
+
   end,
 }
 
